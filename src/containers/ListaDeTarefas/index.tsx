a89a8df46914d68d1a58f8f86cd8b6d768ed815f
Mateus Carvalho <mateus.raimundo1995@gmail.com>
2024-02-05 22:45:08 -0300

    create state of buttons

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,30 +1,43 @@
 import Tarefa from '../../components/Tarefa'
 import * as S from './style'
 
+const tarefas = [
+  {
+    titulo: 'Estudar typescript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Pagar a conta de internet',
+    descricao: 'Baixar fatura no Gmail',
+    prioridade: 'urgente',
+    status: 'concluída'
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'Fazer treino B',
+    prioridade: 'importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => {
   return (
     <S.Container>
       <p>
         2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;
         <ul>
-          <li>
-            <Tarefa />
-          </li>
-          <li>
-            <Tarefa />
-          </li>
-          <li>
-            <Tarefa />
-          </li>
-          <li>
-            <Tarefa />
-          </li>
-          <li>
-            <Tarefa />
-          </li>
-          <li>
-            <Tarefa />
-          </li>
+          {tarefas.map((item) => (
+            <li key={item.titulo}>
+              <Tarefa
+                descricao={item.descricao}
+                prioridade={item.prioridade}
+                status={item.status}
+                titulo={item.titulo}
+              />
+            </li>
+          ))}
         </ul>
       </p>
     </S.Container>
